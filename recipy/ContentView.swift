394b1bb4a9199adb4524cfeba148980269c14102
Joaovictordsl <joaovictordsl00@gmail.com>
2024-02-08 00:00:25 -0300

    Initial Commit

---
+++ recipy/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  recipy
+//
+//  Created by Pedro on 05/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let foodImages = [
+            "chicken",
+            "italian",
+            "pasta",
+            "hamburger2",
+            "tapi"
+        ]
+    
+    
+    var body: some View {
+        ScrollView (showsIndicators: false) {
+            Text("Featured on Uber Eats")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .padding()
+            
+            
+                ForEach(foodImages, id: \.self) { foodImage in
+                    VStack(alignment: .leading){
+                        
+                        Image(foodImage)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 360, height: 150)
+                            .cornerRadius(10)
+                        
+                        VStack(alignment: .leading){
+                            Text("Food")
+                                .fontWeight(.semibold)
+                            Text("%0.99 Delivery Fee | 15 - 30 min")
+                                .foregroundColor(.gray)
+                        }
+                        .font(.subheadline)
+
+                    }
+
+                }
+        }
+        
+        
+    }
+}
+
+#Preview {
+    
+    ContentView()
+}
---
+++ recipy/recipyApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipyApp.swift
+//  recipy
+//
+//  Created by Pedro on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct recipyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
