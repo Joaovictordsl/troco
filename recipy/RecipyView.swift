caff67873fd686fecd7f58441ed905fdf527ac3d
Joaovictordsl <joaovictordsl00@gmail.com>
2024-02-08 22:59:59 -0300

    a

---
+++ recipy/RecipyView.swift
@@ -0,0 +1,22 @@
+//
+//  RecipyView.swift
+//  recipy
+//
+//  Created by Pedro on 08/02/24.
+//
+
+import SwiftUI
+
+struct RecipyView: View {
+    
+    let foodImages = [()]
+    
+    
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+#Preview {
+    RecipyView()
+}
