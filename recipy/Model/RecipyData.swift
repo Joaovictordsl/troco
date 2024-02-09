c3c29ab9279a4ecfc05c9ebafead28ecda16f666
Joaovictordsl <joaovictordsl00@gmail.com>
2024-02-08 23:26:07 -0300

    changes

---
+++ recipy/Model/RecipyData.swift
@@ -0,0 +1,17 @@
+//
+//  RecipyData.swift
+//  recipy
+//
+//  Created by Pedro on 08/02/24.
+//
+
+import Foundation
+
+struct RecipyData {
+    let foodImages: [(key: String, value:String, com: String)] =
+    [("hamburger2","Hamburger","$10.88 | Cheddar BBQ"),
+     ("pasta","Pasta","$22.99 | Italian Pasta"),
+     ("italian","Italian","$15.99 | Ravioli"),
+     ("tapi","Tapioca","$5.99 | Brazilian Tapioca"),
+     ("chicken","Chicken","$4.99 | Chicken Wings")]
+}
---
+++ recipy/View/RecipyView.swift
@@ -0,0 +1,53 @@
+//
+//  RecipyView.swift
+//  recipy
+//
+//  Created by Pedro on 08/02/24.
+//
+
+import SwiftUI
+
+struct RecipyView: View {
+    
+    let recipyData = RecipyData()
+    
+    var body: some View {
+        
+        Text("Uber Eats")
+            .font(.title2)
+        
+        ScrollView (showsIndicators: false) {
+            VStack {
+                ForEach(recipyData.foodImages, id: \.key) { foodImage in
+                    
+                    Button {
+                        
+                    }label: {
+                        Image(foodImage.key)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 360, height: 150)
+                            .cornerRadius(10)
+                    }
+                    VStack (alignment: .leading, spacing: 2) {
+                        Text(foodImage.value)
+                            .font(.subheadline)
+                            .frame(width: 360, height: 20, alignment: .leading)
+                        Text(foodImage.com)
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .frame(width: 360, height: 20, alignment: .leading)
+                        
+                        
+                    }
+                }
+            }
+            
+        }
+        
+    }
+}
+
+#Preview {
+    RecipyView()
+}
